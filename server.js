5c60ccb080da0c3d4e6a16f95636bfb3c0b2f559
Michael Partin <unmykx@gmail.com>
2020-02-14 15:49:47 -0800

    added requires and test routes

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(morgan('tiny'));
+
+app.get('/', (req, res) => {
+    const data = {
+        user: "mike",
+        age: "old enough"
+    }
+    res.json(data);
+});
+
+app.get('/api/user', (req, res) => {
+    const data = {
+        user: "mp",
+        age: "like wine... vinegar"
+    }
+    res.json(data);
+});
+
+app.listen(PORT, console.log(`Server connected @ ${PORT}`));
