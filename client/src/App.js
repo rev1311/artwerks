52a1bfcc81b8d50aad1fa92072633c59a290133d
Michael Partin <unmykx@gmail.com>
2020-02-21 17:17:02 -0800

    added login page component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import LoginPage from './components/LoginPage';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <div className="App">
+        <LoginPage />
+      </div>
   );
 }
 
