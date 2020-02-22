0d241c51e73aa79671e1dcfdaa41c6ae534a18d4
Michael Partin <unmykx@gmail.com>
2020-02-21 17:17:22 -0800

    created events controller

---
+++ client/controllers/events.controller.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+    showEvents: (req, res) => {
+
+        const events = [
+            { name: 'mike', slug: 'mike', description: 'code monkey' },
+            { name: 'ed', slug: 'ed', description: 'y u no work?' },
+            { name: 'cal', slug: 'cal', description: 'taytayparpar' },
+        
+        ];
+
+        res.render('pages/events', { events });
+    
+    }
+
+}
