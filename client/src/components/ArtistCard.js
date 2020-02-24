0e6de72c87755768650aae079921673f0e290546
Michael Partin <unmykx@gmail.com>
2020-02-24 15:59:08 -0800

    added bootstrap layout

---
+++ client/src/components/ArtistCard.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import artists from '../artists.json';
+import { Card } from 'react-bootstrap';
+
+export class ArtistCard extends Component {
+    state = {
+        id: 0,
+        name: "",
+        img: "",
+        title: "",
+        desc: "",
+        artists
+    }
+
+    render() {
+        const artworks = [...this.state.artists];
+
+        return (
+            artworks.map(art => {
+                return (
+            <div>
+            
+                <Card className="bg-dark text-white" key={art.id}>
+                    <Card.Img src={art.img} alt={art.alt} />
+                    <Card.ImgOverlay>
+                    <Card.Title>Title:{art.title}</Card.Title>
+                    <Card.Text>
+                    Description:{art.desc}
+                    </Card.Text>
+                    <Card.Text>By Artist:{art.name}</Card.Text>
+                    </Card.ImgOverlay>
+                </Card>
+                
+            </div>
+        )
+    }));
+}
+};
+
+export default ArtistCard;
