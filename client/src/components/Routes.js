dcf598772e8b5e37bd8b10a5ed2bb53e78e77dec
Michael Partin <unmykx@gmail.com>
2020-02-24 15:59:36 -0800

    added browserrouter for switch

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -2,15 +2,17 @@ import React from 'react';
 import LoginPage from './LoginPage';
 import Artist from './Artist';
 import Main from './Main';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
 
 
 const Routes = () => (
+    <BrowserRouter>
     <Switch>
         <Route exact path="/" component={LoginPage} />
         <Route path="/Main" component={Main} />
         <Route path="/Artist" component={Artist} />
     </Switch>
+    </BrowserRouter>
 )
 
 export default Routes;
