b47e416385ded8f0d4571c4d6b3431fc79363fdf
Michael Partin <unmykx@gmail.com>
2020-02-24 16:00:33 -0800

    created artist profile file

---
+++ client/src/components/Artist.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Artist extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Artist;
