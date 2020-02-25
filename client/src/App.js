9b5e9660ec8a2424ad1d798f1d417ae00cc9aa23
Michael Partin <unmykx@gmail.com>
2020-02-24 16:35:37 -0800

    fixed displaying of all pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import LoginPage from './components/LoginPage';
-import Routes from './components/Routes';
 
 function App() {
   return (
       <div className="App">
         <LoginPage />
-        <Routes />
       </div>
   );
 }
