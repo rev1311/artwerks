0663728d81b2fe0dd35119111dded1971646a88e
Michael Partin <unmykx@gmail.com>
2020-02-26 18:18:40 -0800

    added alt to img

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -4,7 +4,7 @@ export class Nav extends Component {
     render() {
         return (
             <div className="foot">
-                <img src="./assets/logo2.png" style={{ position: "fixed", center: "0", bottom: "0" }} />
+                <img src="./assets/logo2.png" alt="" style={{ position: "fixed", center: "0", bottom: "0" }} />
             </div>
         )
     }
