27caab1e0a9d49e3d1d74ba8f88c1457a0f0a519
Michael Partin <unmykx@gmail.com>
2020-02-28 10:50:01 -0800

    tried to fix link through to carry state

--- client/src/components/ArtistCard.js
+++ client/src/components/ArtistCard.js
@@ -22,12 +22,7 @@ class ArtistCard extends Component {
             artworks.map(art => {
                 return (
             <div>
-                <Link to={{
-                    pathname: `/Artist/${art.id}`,
-                    state: {
-                        id: `${art.id}`
-                    }
-                }}>
+                <Link to={ "/Artist/" + art.id }>
                 <Card className="bg-dark text-white" key={art.id} style={{ margin: "20px", fontFamily: 'Cinzel', boxShadow: "-1px 0 5px 0 rgba(0, 0, 0, .7)" }}>
                     <Card.Img src={art.img} alt={art.alt} style={{  }} />
                     <Card.ImgOverlay>
