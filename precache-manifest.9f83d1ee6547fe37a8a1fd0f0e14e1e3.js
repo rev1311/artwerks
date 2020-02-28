6ee626e0b7ba2996f6dc3b70cb0d7f115764ed88
Michael Partin <unmykx@gmail.com>
2020-02-28 10:55:18 -0800

    Updates

---
+++ precache-manifest.9f83d1ee6547fe37a8a1fd0f0e14e1e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7173f5c973d180d87425f8e1d41cd174",
+    "url": "/artwerks/index.html"
+  },
+  {
+    "revision": "f6d76838d32a9812cde6",
+    "url": "/artwerks/static/css/main.9314e92f.chunk.css"
+  },
+  {
+    "revision": "e5731ebc05f0c21f12a4",
+    "url": "/artwerks/static/js/2.e7a28085.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/artwerks/static/js/2.e7a28085.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6d76838d32a9812cde6",
+    "url": "/artwerks/static/js/main.038c3d5c.chunk.js"
+  },
+  {
+    "revision": "f228856898cf0afba087",
+    "url": "/artwerks/static/js/runtime-main.1edddcf4.js"
+  }
+]);
