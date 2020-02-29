bb981f66c138737f3e97d15bd9e557d089efed91
Michael Partin <unmykx@gmail.com>
2020-02-28 19:46:52 -0800

    changed server routes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express'),
     app = express(),
     PORT = process.env.PORT || 3001;
 
-// app.use(require('./client/routes'));
+app.use(require('./client/routes'));
 app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, console.log(`Server connected @ ${PORT}`)); 
