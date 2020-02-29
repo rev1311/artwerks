a12790ec09a07266ffd58bf60877d2066ae29404
Michael Partin <unmykx@gmail.com>
2020-02-29 00:04:04 -0800

    cleaned out files to fix load issues

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express'),
-    app = express(),
-    PORT = process.env.PORT || 3001;
-
-// app.use(require('./client/routes'));
-app.use(express.static(__dirname + '/public'));
-
-app.listen(PORT, console.log(`Server connected @ ${PORT}`)); 
